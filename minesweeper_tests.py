625e973f69aa33c3b4571c06136bce3c98623d1d
Mel <mel@mel.codes>
2023-01-19 18:44:37 -0800

    adds test for update_row()

--- minesweeper_tests.py
+++ minesweeper_tests.py
@@ -34,6 +34,42 @@ class MinefieldTests(unittest.TestCase):
         self.assertEqual(["*", "*", "*", 1, 0], some_bombs,
                          'Minefield not incrementing properly')
 
+    def test_update_row(self):
+        """
+        This method tests the update_row() method to make sure the values are
+        incrementing properly
+        """
+        minefield = Minefield(5, 5)
+
+        # set test data
+        minefield.data = [[0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0],
+                          ["*", 1, 0, 1, "*"]]
+
+        # update the second row based on the third row (next)
+        minefield.update_row(1)
+        expected_data = [[0, 0, 0, 0, 0],
+                          [1, 1, 0, 1, 1],
+                          ["*", 1, 0, 1, "*"]]
+        self.assertEqual(expected_data, minefield.data,
+                         "Row not updating based on next row.")
+
+        # update the minefield data
+        minefield.data = [[0, 0, 0, 0, 0],
+                          [1, 1, 0, 1, 1],
+                          ["*", 1, 0, 1, "*"],
+                          ["*", 1, 0, 1, "*"]]
+
+        # update the third and fourth rows based on new data
+        minefield.update_row(2)
+        minefield.update_row(3, "prev")
+        expected_data = [[0, 0, 0, 0, 0],
+                         [1, 1, 0, 1, 1],
+                         ["*", 2, 0, 2, "*"],
+                         ["*", 2, 0, 2, "*"]]
+        self.assertEqual(expected_data, minefield.data,
+                         "Row not updating based on previous row.")
+
 
 if __name__ == '__main__':
     unittest.main()
