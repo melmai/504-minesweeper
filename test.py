d4dd0c956d765b6eb2e9725aca7f33e5fd53239e
William Chen <wc1321@uw.edu>
2023-01-20 19:37:41 -0800

    Some more test work

--- test.py
+++ test.py
@@ -6,6 +6,26 @@ from minefield import Minefield
 # output validation test and hint validation test
 
 class minetest(unittest.TestCase):
+    def test_output(self):
+        minesweeper("mines_input_test.txt", "new_output_test.txt")
+        with open("mines_output_test.txt", 'r') as file:
+            original_test_output = file.read()
+        with open("new_output_test.txt", 'r') as file2:
+            new_test_output = file2.read()
+
+        self.assertEqual(original_test_output, new_test_output)
+
+    def test_size(self):
+        field = Minefield(4, 4)
+        self.assertEqual(field.rows, 4)
+        self.assertEqual(field.cols, 4)
+        field = Minefield(100, 100)
+        self.assertEqual(field.rows, 100)
+        self.assertEqual(field.cols, 100)
+        field = Minefield(0, 0)
+        self.assertEqual(field.rows, 0)
+        self.assertEqual(field.cols, 0)
+
     def test_hint_homogenous(self):
         field = Minefield(3, 3)
         field.add_row("...", 0)
@@ -24,12 +44,12 @@ class minetest(unittest.TestCase):
         field.add_row("******", 3)
         field.add_row("******", 4)
         field.add_row("******", 5)
-        self. assertEqual(field._data, [['*', '*', '*', '*', '*', '*'],
-         ['*', '*', '*', '*', '*', '*'],
-         ['*', '*', '*', '*', '*', '*'],
-         ['*', '*', '*', '*', '*', '*'],
-         ['*', '*', '*', '*', '*', '*'],
-         ['*', '*', '*', '*', '*', '*']])
+        self.assertEqual(field._data, [['*', '*', '*', '*', '*', '*'],
+                                       ['*', '*', '*', '*', '*', '*'],
+                                       ['*', '*', '*', '*', '*', '*'],
+                                       ['*', '*', '*', '*', '*', '*'],
+                                       ['*', '*', '*', '*', '*', '*'],
+                                       ['*', '*', '*', '*', '*', '*']])
 
     def test_hint_mt(self):
         field = Minefield(0, 0)
@@ -46,11 +66,14 @@ class minetest(unittest.TestCase):
         field.add_row("*.*", 1)
         field.add_row("***", 2)
         self.assertEqual(field._data, [['*', '*', '*'], ['*', 8, '*'], ['*', '*', '*']])
+
+    def test_two(self):
+
         field = Minefield(3, 3)
         field.add_row(".**", 0)
         field.add_row("*.*", 1)
         field.add_row("***", 2)
-        self.assertEqual(field._data,[[2, '*', '*'], ['*', 7, '*'], ['*', '*', '*']])
+        self.assertEqual(field._data, [[2, '*', '*'], ['*', 7, '*'], ['*', '*', '*']])
         field = Minefield(3, 3)
         field.add_row("**.", 0)
         field.add_row("*.*", 1)
@@ -68,8 +91,8 @@ class minetest(unittest.TestCase):
         field.add_row("*.*..*..**..", 1)
         field.add_row("*....*..**..", 2)
         self.assertEqual(field._data, [[2, '*', '*', 2, 2, '*', 2, 1, 3, '*', 2, 0],
- ['*', 5, '*', 2, 3, '*', 3, 2, '*', '*', 3, 0],
- ['*', 3, 1, 1, 2, '*', 2, 2, '*', '*', 2, 0]])
+                                       ['*', 5, '*', 2, 3, '*', 3, 2, '*', '*', 3, 0],
+                                       ['*', 3, 1, 1, 2, '*', 2, 2, '*', '*', 2, 0]])
 
 
 if __name__ == '__main__':
